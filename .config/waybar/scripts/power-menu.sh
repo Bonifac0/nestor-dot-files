90df314b9a497b3bba0f3f7c280e5e41c91a9056
Bonifac <kolenomi@seznam.cz>
2026-02-19 14:50:23 +0100

    small powermenu tweek

--- .config/waybar/scripts/power-menu.sh
+++ .config/waybar/scripts/power-menu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-op=$(echo -e "  Shutdown\n󰑐  Reboot\n  Suspend\n󰍃  Logout" | wofi -i --dmenu --width 200 --height 265 --cache-file /dev/null)
+op=$(echo -e "  Shutdown\n󰑐  Reboot\n  Suspend\n󰍃  Logout" | wofi -i --dmenu --width 200 --height 285 --cache-file /dev/null)
 
 case $op in
     "  Shutdown")
